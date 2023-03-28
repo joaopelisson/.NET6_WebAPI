990b39f70c486588bfe1d520c9184faa144cad93
joaopelisson <joaopedro.devbr@gmail.com>
2023-03-27 22:07:08 -0300

    Fluent API

--- Program.cs
+++ Program.cs
@@ -80,6 +80,16 @@ public class ApplicationDbContext: DbContext{
     
     public DbSet<Product> Products { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Product>()
+        .Property(p => p.Description).HasMaxLength(500).IsRequired(false);
+        builder.Entity<Product>()
+        .Property(p => p.Name).HasMaxLength(120).IsRequired();
+        builder.Entity<Product>()
+        .Property(p => p.Code).HasMaxLength(20).IsRequired();
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlServer(
             "Server=localhost;Database=Products;User Id=sa;Password=###@;MultipleActiveResultSets=true;Encrypt=YES;TrustServerCertificate=YES"
