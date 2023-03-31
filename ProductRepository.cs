b4597f4410bd2b2c4fcb01818d6c75be8fa7aec8
joaopelisson <joaopedro.devbr@gmail.com>
2023-03-31 09:05:33 -0300

    Connection string in configuration file and best practices enforcement

---
+++ ProductRepository.cs
@@ -0,0 +1,22 @@
+
+//simulating a database
+public static class ProductRepository {
+    public static List<Product> Products { get; set; } = Products = new List<Product>();
+
+    public static void Init(IConfiguration configuration){
+        var products = configuration.GetSection("Products").Get<List<Product>>();
+        Products = products;
+    }
+
+    public static void add(Product product) {
+        Products.Add(product);
+    }
+    public static Product GetBy(string code){
+        return Products.FirstOrDefault(p => p.Code == code);
+    }
+
+    public static void Remove(Product product){
+        Products.Remove(product);
+    }
+
+}
---
+++ Tag.cs
@@ -0,0 +1,7 @@
+public class Tag{
+    public int Id { get; set; }
+
+    public string Name {get; set;}
+
+    public int ProductId { get; set; }
+}
